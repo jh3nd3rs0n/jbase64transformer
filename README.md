771689ea28e895ba80b766aab5c677678751f3d2
Jonathan K. Henderson <j0n4th4n.h3nd3rs0n@gmail.com>
2019-02-12 20:54:40 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,88 @@
+# JBase64Transformer
+
+JBase64Transformer is a Java implementation of GNU's base64 utility and an example of using ArgMatey.
+
+## Contents
+
+- <a href="#requirements">Requirements</a>
+
+- <a href="#building">Building</a>
+
+- <a href="#starting">Starting JBase64Transformer</a>
+
+- <a href="#usage">Usage</a>
+
+<a name="requirements"></a>
+
+## Requirements
+
+- Apache Maven&#8482; 3.3.9 or higher 
+
+- Java&#8482; SDK 1.8 or higher
+
+- Local Maven project dependencies (requires download and installation)
+
+  - [argmatey](https://github.com/jh3nd3rs0n/argmatey)
+
+<a name="building"></a>
+
+## Building
+
+To build and package JBase64Transformer as an executable jar file, run the following commands:
+
+<pre>
+
+$ cd jbase64transformer
+$ mvn package
+
+</pre>
+
+<a name="starting"></a>
+
+## Starting JBase64Transformer 
+
+To start JBase64Transformer, you can run the following command:
+
+<pre>
+
+$ mvn exec:java -Dexec.args="[OPTION]... [FILE]"
+
+</pre>
+
+Replace `[OPTION]...` with command line options described in the usage below and replace `[FILE]` with the file you would like transformed.
+
+If you have JBase64Transformer packaged as an executable jar file, you can run the following command:
+
+<pre>
+
+$ java -jar jbase64transformer-1.0-SNAPSHOT.jar [OPTION]... [FILE]
+
+</pre>
+
+Again, replace `[OPTION]...` with command line options described in the usage below and replace `[FILE]` with the file you would like transformed.
+
+<a name="usage"></a>
+
+## Usage
+
+<pre>
+
+Usage: jbase64transformer.Base64Transformer [OPTION]... [FILE]
+Base64 encode or decode FILE, or standard input, to standard output.
+
+OPTIONS:
+  -d, --decode
+      decode data
+  -i, --ignore-garbage
+      when decoding, ignore non-alphabet characters
+  -w COLS, --wrap=COLS
+      wrap encoded lines after COLS character (default 76).
+      Use 0 to disable line wrapping
+  --help
+      display this help and exit
+  --version
+      display version information and exit
+
+With no FILE, or when FILE is -, read standard input.
+
+</pre>
---
+++ src/main/java/jbase64transformer/Base64Transformer.java
@@ -0,0 +1,229 @@
+package jbase64transformer;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+import java.util.Arrays;
+
+import argmatey.ArgsParser;
+import argmatey.GnuLongOption;
+import argmatey.Option;
+import argmatey.OptionArgSpec;
+import argmatey.Options;
+import argmatey.ParseResult;
+import argmatey.PosixOption;
+import argmatey.StringConverter;
+
+public final class Base64Transformer {
+	
+	private static void decode(
+			final Reader reader, 
+			final OutputStream out, 
+			final boolean ignoreGarbage) throws IOException {
+		String base64Alphabet = 
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+		String whitespaceNewlines = "\r\n";
+		final int groupSize = 4;
+		StringBuilder sb = new StringBuilder();
+		java.util.Base64.Decoder decoder = java.util.Base64.getDecoder();
+		while (true) {
+			int c = reader.read();
+			if (c == -1) { break; }
+			if (base64Alphabet.indexOf(c) == -1) {
+				if (whitespaceNewlines.indexOf(c) == -1 && !ignoreGarbage) {
+					throw new IOException("non-alphabet character found");
+				}
+				continue;
+			}
+			sb.append((char) c);
+			if (sb.length() < groupSize) {
+				continue;
+			}
+			out.write(decoder.decode(sb.toString()));
+			sb = new StringBuilder();
+		}
+		out.flush();
+	}
+	
+	private static void encode(
+			final InputStream in,
+			final Writer writer,
+			final int numOfColumnsLimit) throws IOException {
+		final int groupSize = 3;
+		java.util.Base64.Encoder encoder = java.util.Base64.getEncoder();
+		int numOfColumns = 0;
+		String lineSeparator = System.getProperty("line.separator");
+		while (true) {
+			byte[] b = new byte[groupSize];
+			int newLength = in.read(b);
+			if (newLength == -1) { break; }
+			b = Arrays.copyOf(b, newLength);
+			String encoded = encoder.encodeToString(b);
+			if (numOfColumnsLimit > 0) {
+				int encodedLength = encoded.length();
+				numOfColumns += encodedLength;
+				if (numOfColumns >= numOfColumnsLimit) {
+					int diff = numOfColumns - numOfColumnsLimit;
+					StringBuilder sb = new StringBuilder(encoded);
+					sb.insert(encodedLength - diff, lineSeparator);
+					encoded = sb.toString();
+					numOfColumns = diff;
+				}
+			}
+			writer.write(encoded);
+		}
+		writer.write(lineSeparator);
+		writer.flush();
+	}
+	
+	public static void main(final String[] args) {
+		String lineSeparator = System.getProperty("line.separator");
+		Option decodeOption = new PosixOption.Builder('d')
+				.doc("decode data")
+				.otherBuilders(new GnuLongOption.Builder("decode"))
+				.build();
+		Option ignoreGarbageOption = new PosixOption.Builder('i')
+				.doc("when decoding, ignore non-alphabet characters")
+				.otherBuilders(new GnuLongOption.Builder("ignore-garbage"))
+				.build();
+		Option wrapOption = new PosixOption.Builder('w')
+				.doc("wrap encoded lines after COLS character (default 76)."
+						+ lineSeparator 
+						+ "      Use 0 to disable line wrapping")
+				.optionArgSpec(new OptionArgSpec.Builder()
+						.name("COLS")
+						.stringConverter(new StringConverter() {
+
+							@Override
+							public Object convert(String string) {
+								String message = String.format(
+										"must be an integer between "
+										+ "%s (inclusive) and %s (inclusive)", 
+										0,
+										Integer.MAX_VALUE);
+								int intValue;
+								try {
+									intValue = Integer.parseInt(string);
+								} catch (NumberFormatException e) {
+									throw new IllegalArgumentException(
+											message, e);
+								}
+								if (intValue < 0) {
+									throw new IllegalArgumentException(message);
+								}
+								return Integer.valueOf(intValue);
+							}
+							
+						})
+						.type(Integer.class)
+						.build())
+				.otherBuilders(new GnuLongOption.Builder("wrap"))
+				.build();
+		Option helpOption = new GnuLongOption.Builder("help")
+				.doc("display this help and exit")
+				.special(true)
+				.build();
+		Option versionOption = new GnuLongOption.Builder("version")
+				.doc("display version information and exit")
+				.special(true)
+				.build();
+		Options options = new Options(
+				decodeOption, 
+				ignoreGarbageOption, 
+				wrapOption, 
+				helpOption, 
+				versionOption);
+		ArgsParser argsParser = ArgsParser.newInstance(args, options, false);
+		String programName = Base64Transformer.class.getName();
+		String programVersion = "1.0";
+		String suggestion = String.format("Try '%s %s' for more information", 
+				programName, helpOption);
+		boolean decode = false;
+		boolean ignoreGarbage = false;
+		final int defaultNumOfColumnsLimit = 76;
+		int numOfColumnsLimit = defaultNumOfColumnsLimit;
+		InputStream in = null;
+		while (argsParser.hasNext()) {
+			ParseResult parseResult = null;
+			try { 
+				parseResult = argsParser.parseNext(); 
+			} catch (RuntimeException e) {
+				System.err.printf("%s: %s%n%s%n", 
+						programName, e.toString(), suggestion);
+				System.exit(-1);
+			}
+			if (parseResult.hasOptionFrom(helpOption)) {
+				System.out.printf("Usage: %s [OPTION]... [FILE]%n", 
+						programName);
+				System.out.printf("Base64 encode or decode FILE, or standard "
+						+ "input, to standard output.%n%n");
+				System.out.println("OPTIONS:");
+				options.printHelpText();
+				System.out.printf("%n%nWith no FILE, or when FILE is -, read "
+						+ "standard input.%n");
+				return;
+			}
+			if (parseResult.hasOptionFrom(versionOption)) {
+				System.out.printf("%s %s%n", programName, programVersion);
+				return;
+			}
+			if (parseResult.hasOptionFrom(decodeOption)) {
+				decode = true;
+			}
+			if (parseResult.hasOptionFrom(ignoreGarbageOption)) {
+				ignoreGarbage = true;
+			}
+			if (parseResult.hasOptionFrom(wrapOption)) {
+				numOfColumnsLimit = parseResult.getOptionArg().getTypeValue(
+						Integer.class).intValue();
+			}
+			if (parseResult.hasNonparsedArg()) {
+				String arg = parseResult.getNonparsedArg();
+				if (in != null) {
+					System.err.printf("%s: extra operand '%s'%n%s%n", 
+							programName, arg, suggestion);
+					System.exit(-1);
+				}
+				if (arg.equals("-")) {
+					in = System.in;
+				} else {
+					File file = new File(arg);
+					try {
+						in = new FileInputStream(file);
+					} catch (FileNotFoundException e) {
+						System.err.printf("%s: %s%n%s%n", 
+								programName, e.toString(), suggestion);
+						System.exit(-1);
+					}
+				}
+			}
+		}
+		if (in == null) { in = System.in; }
+		if (decode) {
+			Reader reader = new InputStreamReader(in);
+			try {
+				decode(reader, System.out, ignoreGarbage);
+			} catch (IOException e) {
+				System.err.printf("%s: %s%n", programName, e.toString());
+				e.printStackTrace(System.err);
+				System.exit(-1);
+			}
+		} else {
+			Writer writer = new OutputStreamWriter(System.out);
+			try {
+				encode(in, writer, numOfColumnsLimit);
+			} catch (IOException e) {
+				System.err.printf("%s: %s%n", programName, e.toString());
+				e.printStackTrace(System.err);
+				System.exit(-1);
+			}
+		}
+	}
+}
